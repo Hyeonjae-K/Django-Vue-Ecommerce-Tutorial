5c19f9cd2a4527204fb2513473a718bb21b3a0ff
Hyeonjae-K <href.hjk@gmail.com>
2022-09-03 00:48:22 +0900

    View cart

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,6 +4,7 @@ import HomeView from '../views/HomeView.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
 
 const routes = [
   {
@@ -32,8 +33,13 @@ const routes = [
   {
     path: '/search',
     name: 'Search',
-    component: Search
+    component: Search,
   },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart,
+  }
 ]
 
 const router = createRouter({
