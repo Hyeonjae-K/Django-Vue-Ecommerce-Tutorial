c1e3b36c687cff68dd5dbe868cb421f1b3a52f06
Hyeonjae-K <href.hjk@gmail.com>
2022-09-12 22:12:47 +0900

    Done

---
+++ backend/config/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/', include('product.urls')),
+    path('api/v1/', include('order.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
